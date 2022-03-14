453a3cd6f600ff09332534561cb61712e57289e8
Souheil Chelfouh <trollfot@gmail.com>
2022-03-14 02:21:30 +0100

    Using a real manager to send emails

--- src/kavallerie/pipes/emailer.py
+++ src/kavallerie/pipes/emailer.py
@@ -7,6 +7,8 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from kavallerie.response import Response
 from kavallerie.pipeline import Handler, MiddlewareFactory
+from transaction.interfaces import IDataManager
+from zope.interface import implementer
 
 
 class SMTPConfiguration(t.NamedTuple):
@@ -23,6 +25,7 @@ class Courrier:
     def __init__(self, config: SMTPConfiguration):
         self.config = config
         self.queue = deque()
+        self.server = None
 
     @staticmethod
     def format_email(origin, target, subject, text, html=None):
@@ -45,15 +48,22 @@ class Courrier:
         return msg
 
     def connect(self):
-        server = smtplib.SMTP(self.config.host, str(self.config.port))
-        server.set_debuglevel(self.config.debug)
-        code, response = server.ehlo()
-        if code < 200 or code >= 300:
-            raise RuntimeError(
-                'Error sending EHLO to the SMTP server '
-                f'(code={code}, response={response})'
-            )
-        return server
+        if self.server is None:
+            server = smtplib.SMTP(self.config.host, str(self.config.port))
+            server.set_debuglevel(self.config.debug)
+            code, response = server.ehlo()
+            if code < 200 or code >= 300:
+                raise RuntimeError(
+                    'Error sending EHLO to the SMTP server '
+                    f'(code={code}, response={response})'
+                )
+            self.server = server
+        return self.server
+
+    def disconnect(self):
+        if self.server is not None:
+            self.server.close()
+            self.server = None
 
     def clear(self):
         self.queue.clear()
@@ -82,7 +92,48 @@ class Courrier:
                 server.sendmail(
                     email['From'], email['To'], email.as_string())
         finally:
-            server.close()
+            self.disconnect()
+
+
+@implementer(IDataManager)
+class MailDataManager:
+
+    def __init__(self, courrier: Courrier, manager):
+        self.courrier = courrier
+        self.transaction_manager = manager
+
+    def commit(self, txn):
+        pass
+
+    def abort(self, txn):
+        self.courrier.disconnect()
+
+    def sortKey(self):
+        return str(id(self))
+
+    def beforeCompletion(self, txn):
+        pass
+
+    afterCompletion = beforeCompletion
+
+    def tpc_begin(self, txn, subtransaction=False):
+        assert not subtransaction
+
+    def tpc_vote(self, txn):
+        self.courrier.connect()
+
+    def tpc_finish(self, txn):
+        try:
+            self.courrier.exhaust()
+        except Exception as exc:
+            # Any exceptions here can cause database corruption.
+            # Better to protect the data and potentially miss emails than
+            # leave a database in an inconsistent state which requires a
+            # guru to fix.
+            log.exception(
+                f"Failed in tpc_finish for {self.courrier.exhaust!r}")
+
+    tpc_abort = abort
 
 
 class Mailer(MiddlewareFactory):
@@ -95,14 +146,16 @@ class Mailer(MiddlewareFactory):
                  globalconf: t.Optional[t.Mapping] = None):
 
         def emailer_middleware(request):
-            request.utilities['courrier'] = Courrier(self.config)
-            response = handler(request)
-            if response.status < 400:
-                tm = request.utilities.get('transaction_manager')
-                if tm is None or not tm.isDoomed():
-                    request.utilities['courrier'].exhaust()
+            courrier = request.utilities['courrier'] = Courrier(self.config)
+            tm = request.utilities.get('transaction_manager')
+            if tm is not None and not tm.isDoomed():
+                tm.get().join(MailDataManager(courrier, tm))
 
-            del request.utilities['courrier']
+            response = handler(request)
+
+            if tm is None and response.status < 400:
+                request.utilities['courrier'].exhaust()
+                del request.utilities['courrier']
             return response
 
         return emailer_middleware
