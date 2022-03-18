83a4cb9ce846261b02ca342cb3d2feee2087510a
Souheil CHELFOUH <trollfot@gmail.com>
2022-03-18 01:44:47 +0100

    Session no longer written if the session is new and not modified.

--- src/kavallerie/pipes/testing.py
+++ src/kavallerie/pipes/testing.py
@@ -1,4 +1,5 @@
 import typing as t
+import http_session
 from kavallerie.pipes.authentication import Source
 from kavallerie.request import Request, User
 
@@ -22,3 +23,31 @@ class DictSource(Source):
             user = User()
             user.id = uid
             return user
+
+
+class MemorySessionStore(http_session.Store):
+
+    def __init__(self):
+        self._store = {}
+
+    def touch(self, sid):
+        pass
+
+    def get(self, sid):
+        """We return a copy, to avoid mutability by reference."""
+        from copy import deepcopy
+
+        data = self._store.get(sid)
+        if data is not None:
+            return deepcopy(data)
+        return data
+
+    def set(self, sid, session):
+        self._store[sid] = session
+
+    def clear(self, sid):
+        if sid in self._store:
+            self._store[sid].clear()
+
+    def delete(self, sid):
+        del self._store[sid]
