1c39b198a690993a61e6f2b71245631ffc6940d6
Souheil Chelfouh <trollfot@gmail.com>
2022-03-25 23:27:08 +0100

    Updated tests

--- tests/plugins/test_plugins.py
+++ tests/plugins/test_plugins.py
@@ -3,19 +3,10 @@ from kavallerie.plugins import Plugin, Plugins
 from kavallerie.app import Application
 
 
-def test_plugins_dependencies_order():
-    transaction = Plugin('transaction')
-    sql = Plugin('sql', dependencies=[transaction])
-    myplugin = Plugin('myplugin', dependencies=[sql])
-    plugins = Plugins([myplugin])
-
-    assert plugins.ordered == ('transaction', 'sql', 'myplugin')
-
-
 def test_unknown_dependency():
     plugins = Plugins([])
     with pytest.raises(LookupError):
-        plugins.apply(None, 'unknown')
+        plugins.install_by_name(None, 'unknown')
 
 
 def test_plugins_dependencies():
@@ -25,10 +16,8 @@ def test_plugins_dependencies():
     myplugin = Plugin('myplugin', dependencies=[sql])
     plugins = Plugins([myplugin, useless])
 
-    assert plugins.ordered == ('transaction', 'sql', 'useless', 'myplugin')
-
     app = Application()
-    plugins.apply(app, 'myplugin', 'useless')
+    plugins.install_by_name(app, 'myplugin', 'useless')
     assert app.__installed_plugins__ == {
         'sql', 'useless', 'transaction', 'myplugin'
     }
