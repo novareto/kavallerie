3939f9cba420bf1e3511eee4e9b3d4597de72e03
Souheil Chelfouh <trollfot@gmail.com>
2022-03-29 15:51:25 +0200

    Preparing release

--- setup.py
+++ setup.py
@@ -1,60 +1,28 @@
-import os
-from setuptools import setup, find_packages
-
-
-version = "0.1.dev0"
-
-install_requires = [
-    'frozen_box',
-    'horseman',
-    'http-session >= 0.2',
-    'importscan',
-    'itsdangerous',
-    'prejudice',
-    'roughrider.cors',
-    'roughrider.routing',
-    'transaction',
-    'zope.interface',
-]
-
-test_requires = [
-    'WebTest',
-    'pytest',
-    'smtpdfix',
-    'frozendict',
-    'sqlalchemy.orm',
-    'zope.sqlalchemy',
-]
+from setuptools import setup
 
 
 setup(
     name='kavallerie',
-    version=version,
-    author='Novareto GmbH & Souheil Chelfouh',
-    author_email='',
-    url='https://github.com/HorsemanWSGI/kavallerie',
-    download_url='http://pypi.python.org/pypi/kavallerie',
-    description='Full-blown WSGI Framework using Horseman.',
-    long_description=(open("README.rst").read() + "\n" +
-                      open(os.path.join("docs", "HISTORY.rst")).read()),
-    license='ZPL',
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Internet :: WWW/HTTP :: WSGI',
+    install_requires = [
+        'frozen_box',
+        'horseman',
+        'http-session >= 0.2',
+        'importscan',
+        'itsdangerous',
+        'prejudice',
+        'roughrider.cors',
+        'roughrider.routing',
+        'transaction',
+        'zope.interface',
     ],
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    setup_requires=[],
-    tests_require=test_requires,
     extras_require={
-        'test': test_requires,
+        'test': [
+            'WebTest',
+            'pytest',
+            'smtpdfix',
+            'frozendict',
+            'sqlalchemy.orm',
+            'zope.sqlalchemy',
+        ]
     }
 )
