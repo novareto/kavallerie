5243431f0aee9fed9ad20201eeb9490bb1a0df6b
Souheil Chelfouh <trollfot@gmail.com>
2026-02-23 15:53:59 +0100

    Using pyproject.toml

--- setup.py
+++ setup.py
@@ -1,33 +1,3 @@
 from setuptools import setup
 
-
-setup(
-    name='kavallerie',
-    install_requires = [
-        'frozendict',
-        'gelidum',
-        'horseman >= 0.6',
-        'http-session >= 0.2',
-        'importscan',
-        'itsdangerous',
-        'prejudice',
-        'postrider',
-        'roughrider.cors',
-        'roughrider.routing',
-        'transaction',
-        'zope.interface',
-        'importlib-metadata >= 6.5',
-        'jsonschema-rs',
-    ],
-    extras_require={
-        'test': [
-            'WebTest',
-            'pytest',
-            'pyhamcrest',
-            'smtpdfix',
-            'sqlalchemy.orm',
-            'zope.sqlalchemy',
-            'freezegun',
-        ]
-    }
-)
+setup()
